1781f0a5eb1d25ec1e9855d2ff81dd11d88bab2d
[shrey mehwala] <shreymehwala@gmail.com>
2026-03-10 17:47:58 +0530

    removed the think portion in the result for thinking models, still visible in the test notebook

--- main.py
+++ main.py
@@ -2,24 +2,43 @@ from langchain_ollama.llms import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
 from vector_db import retriever
 
-model = OllamaLLM(model="llama3.1:8b")
+import re
+regex_pattern = r"<think>[\s\S]*?<\/think>\n?\n?"   #to remove the thinking part
+
+import os
+from dotenv import load_dotenv,dotenv_values
+load_dotenv()
+
+m2 = os.getenv("MODEL_2")
+print(m2)
+
+model = OllamaLLM(model=m2)
 
 template = """
-You are an expert in restaurants recommendation and thus will be asked about various type of restaurants and where the best type of particular food will be available, etc.
-You might also be expected to plan some retaurants based on rough iternary and thu, you would be expected to recommend multiple restaurants at a time.
+You are an expert in recommendation for the restaurant named "Pizza Place" and thus will be asked about various type of restaurants and where the best type of particular food will be available, etc.
+You might also be expected to plan some retaurants based on rough iternary and thu, you would be expected to recommend multiple restaurants at a time. 
 
 Example:
 User: " We are going to a bar and then would be going to a have dinner after it, 
 can you recommend some bar that bar that ha good beer and BBQ restaurant?"
 Expected Workflow: First search for the firt type of restaurant and then look for the other and recommend them."
 
+If the user asks for a particular style of pizza/dish, search for the pizza or dish that fits the description and include its name in the result. 
+If you cannot find the appropriate result, inform the user about it.
+
+Example: 
+User: Do they have any vegan pizza.
+Agent-action: 1.) first search for the required pizza, in this case you got a result with name "tropical blast".
+If you also obtain the name of the dish/ pizza, be sure to include the name of the pizza/dish in the final result.
+
+
 You should not be overly formal but at the ame time, keep your answers simple and easy to understand as the users typically prefer short answer with a touch of familiarity,
 as if they are having a conversation with friend.
-Do not hallucinate and give at-most 3 recommendationn with the information such a the place's name .
+Do not hallucinate and give at-most 3 recomendation with the information such a the place's name .
 
 Here are some relevant reviews: {reviews}
 
-Here is the quesstion to answer: {questions}
+Here is the question to answer: {questions}
 """
 
 prompt = ChatPromptTemplate.from_template(template)
@@ -39,5 +58,6 @@ while True:
     
     reviews = retriever.invoke(question)
     result = chain.invoke({"reviews":reviews,"questions":question})
+    result = re.sub(regex_pattern, '', result)
     print(result)
     print("\n\n------------------------------")
